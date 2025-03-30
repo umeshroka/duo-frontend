6bf7e6f7a2e90835ca024b2b94d621875a813213
umeshroka <umeshroka35@gmail.com>
2025-03-30 13:37:59 +0800

    fix: get rid of mobile menu and add sign up button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,16 @@
 // src/components/NavBar.jsx
-import { useState, useContext } from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router'; 
 import { UserContext } from '../contexts/UserContext';
 import { ModalContext } from '../contexts/modalContext';
 
 const NavBar = () => {
   const { user, setUser } = useContext(UserContext);
-  const { openSignIn } = useContext(ModalContext);
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const { openSignIn, openSignUp } = useContext(ModalContext);
 
   const handleSignOut = () => {
     localStorage.removeItem('token');
     setUser(null);
-    setMobileMenuOpen(false);
   };
 
   return (
@@ -21,15 +19,7 @@ const NavBar = () => {
         {/* Logo */}
         <Link to="/">DUO</Link>
         
-        {/* Mobile menu button */}
-        <button 
-          onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
-          aria-label="Toggle menu"
-        >
-          {mobileMenuOpen ? 'Close' : 'Menu'}
-        </button>
-        
-        {/* Desktop navigation links */}
+        {/* Navigation links */}
         <div>
           <Link to="/artists">Artists</Link>
           <Link to="/artworks">Artworks</Link>
@@ -45,41 +35,13 @@ const NavBar = () => {
               <button onClick={handleSignOut}>Sign Out</button>
             </div>
           ) : (
-            <button onClick={openSignIn}>Sign In</button>
+            <div>
+              <button onClick={openSignIn}>Sign In</button>
+              <button onClick={openSignUp}>Sign Up</button>
+            </div>
           )}
         </div>
       </div>
-      
-      {/* Mobile menu */}
-      {mobileMenuOpen && (
-        <div>
-          <Link to="/artists" onClick={() => setMobileMenuOpen(false)}>Artists</Link>
-          <Link to="/artworks" onClick={() => setMobileMenuOpen(false)}>Artworks</Link>
-          <Link to="/masterclasses" onClick={() => setMobileMenuOpen(false)}>Masterclasses</Link>
-          <Link to="/services" onClick={() => setMobileMenuOpen(false)}>Services</Link>
-          <Link to="/editorials" onClick={() => setMobileMenuOpen(false)}>Editorials</Link>
-          <Link to="/playground" onClick={() => setMobileMenuOpen(false)}>AI Playground</Link>
-          
-          {/* Auth Section */}
-          <div>
-            {user ? (
-              <div>
-                <div>{user.email}</div>
-                <button onClick={handleSignOut}>Sign Out</button>
-              </div>
-            ) : (
-              <button 
-                onClick={() => {
-                  openSignIn();
-                  setMobileMenuOpen(false);
-                }}
-              >
-                Sign In
-              </button>
-            )}
-          </div>
-        </div>
-      )}
     </nav>
   );
 };
